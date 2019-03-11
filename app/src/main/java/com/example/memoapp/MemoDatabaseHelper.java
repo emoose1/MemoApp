3266bface9eba9d3794144284aa543e9524712a2
Emmanuel M <44449134+NEMuse@users.noreply.github.com>
2019-03-11 07:50:47 -0400

    first MemoDatabaseHelper commit

---
+++ app/src/main/java/com/example/memoapp/MemoDatabaseHelper.java
@@ -0,0 +1,35 @@
+package com.example.memoapp;
+
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.DatabaseErrorHandler;
+import android.util.Log;
+import android.content.Context;
+import android.content.ContentValues;
+
+
+
+public class MemoDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String TAG = "MemoDatabaseHelper";
+    private static final String TABLE_NAME = "memo_table";
+    private static final int DATABASE_VERSION = 1;
+    private static final String COL1 = "priorityID";
+    private static final String COL2 = "memoText";
+
+
+    public MemoDatabaseHelper(Context context){
+        super(context, TABLE_NAME, null, DATABASE_VERSION);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}

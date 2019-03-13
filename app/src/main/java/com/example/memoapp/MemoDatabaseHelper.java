fee63dc055db9023a52ad5318102bf2245537e0f
emmanuel muse <emmanuelmuse305@gmail.com>
2019-03-13 11:24:27 -0400

    change to column names

--- app/src/main/java/com/example/memoapp/MemoDatabaseHelper.java
+++ app/src/main/java/com/example/memoapp/MemoDatabaseHelper.java
@@ -14,15 +14,15 @@ public class MemoDatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "memo.db";
     private static final int DATABASE_VERSION = 1;
-    /*private static final String COL1 = "priorityID";
-    private static final String COL2 = "memoText";
-    private static final String COL3 = "memoDate";
-    private int _id;
-    */
+    private static final String COLUMN_MemoID = "memoID";
+    private static final String COLUMN_MemoText = "memoNotes";
+    private static final String COLUMN_MemoDate = "memoDate";
+    private static final String COLUMN_PriorityID = "priorityID";
+
 
     private static final String CREATE_TABLE_MEMO =
-            "create table memo (_id integer primary key autoincrement, "
-            + " memoText text not null, memoDate Calendar, "
+            "create table memo (memoID integer primary key autoincrement, "
+            + " memoNotes text not null, memoDate Calendar, "
             + "priorityID int);";
 
     public MemoDatabaseHelper(Context context){

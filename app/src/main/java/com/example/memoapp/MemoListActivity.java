e482c118e1b53c40767a714db0a0dc6d8bf28499
salyateem13 <31292258+salyateem13@users.noreply.github.com>
2019-03-13 17:13:59 -0400

    added onResume to load preferences

--- app/src/main/java/com/example/memoapp/MemoListActivity.java
+++ app/src/main/java/com/example/memoapp/MemoListActivity.java
@@ -1,9 +1,13 @@
 package com.example.memoapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MemoListActivity extends AppCompatActivity {
 
@@ -29,5 +33,31 @@ public class MemoListActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        String sortBy = getSharedPreferences("MyContactListPreferences", Context.MODE_PRIVATE).getString("sortfield", "contactname");
+        String sortOrder = getSharedPreferences("MyContactListPreferences", Context.MODE_PRIVATE).getString("sortorder", "ASC");
+
+        ContactDataSource ds = new ContactDataSource(this);
+        try {
+            ds.open();
+            contacts = ds.getContacts(sortBy, sortOrder);
+            ds.close();
+            if (contacts.size() > 0) {
+                ListView listView = getListView();
+                adapter = new ContactAdapter(this, contacts);
+                listView.setAdapter(adapter);
+            }
+            else {
+                Intent intent = new Intent(ContactListActivity.this, ContactActivity.class);
+                startActivity(intent);
+            }
+        }
+        catch (Exception e) {
+            Toast.makeText(this, "Error retrieving contacts", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
